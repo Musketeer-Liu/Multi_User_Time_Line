6d29296a3fe31ffdb3239409159d77120c72bad8
Musketeer.Liu <Musketeer.Liu@gmail.com>
2017-04-29 13:44:38 -0700

    Debug

--- handlers/blog_handler.py
+++ handlers/blog_handler.py
@@ -28,7 +28,7 @@ class BlogHandler(webapp2.RequestHandler):
     def logout(self):
         self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
 
-    def initialize(self, *a, *kw):
+    def initialize(self, *a, **kw):
         webapp2.RequestHandler.initialize(self, *a, **kw)
         uid = self.read_secure_cookie('user_id')
         self.user = uid and User.by_id(int(uid))
